a9404375ff45615a1e1decec70aafceecd9147f6
Firuza <finama10@gmail.com>
2018-07-19 19:53:44 -0400

    added jquery

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,10 +1,51 @@
-$(function)() {
-    console.log("Hello!")
+$('#contact-button').click(function() {
+    //const preview = $('#subject').val();
+    
+    //$('#subjectPreview').text(preview);
+    alert("Thank you for your question.");
+  
+})
 
-    $("#submit").click(function(){
-        submit();
-    });
+
+$( window ).resize(function() {
+  if ($(window).width() <= 800){	
+    //do something here
+    console.log("I have resized to under 800");
+    
+	}
 });
-function submit() {
-    alert("Your application have been submitted. Thank you!");
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
